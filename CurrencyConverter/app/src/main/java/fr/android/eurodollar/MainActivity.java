e46300888cd496f59273e6bac96826b744dcaa77
QV NGUYEN <qv.nguyen00@gmail.com>
2023-02-09 17:09:43 +0100

    done exo 1

--- CurrencyConverter/app/src/main/java/fr/android/eurodollar/MainActivity.java
+++ CurrencyConverter/app/src/main/java/fr/android/eurodollar/MainActivity.java
@@ -3,6 +3,10 @@ package fr.android.eurodollar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void myClickHandler(View view) {
+        if (view.getId() == R.id.button1) {
+            EditText text = (EditText) findViewById(R.id.editText1);
+            RadioButton euroButton = (RadioButton) findViewById(R.id.radioButton1);
+            RadioButton dollarButton = (RadioButton) findViewById(R.id.radioButton2);
+            if (text.getText().length() == 0) {
+                Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+                return;
+            }
+            float inputValue = Float.parseFloat(text.getText().toString());
+            if (euroButton.isChecked()) {
+                text.setText(String.valueOf(convertDollarToEuro(inputValue)));
+                euroButton.setChecked(false);
+                dollarButton.setChecked(true);
+            } else {
+                text.setText(String.valueOf(convertEuroToDollar(inputValue)));
+                dollarButton.setChecked(false);
+                euroButton.setChecked(true);
+            }
+        }
+    }
+
+    private float convertDollarToEuro(float dollar) {
+        return dollar * 0.93f;
+    }
+    private float convertEuroToDollar(float euro) {
+        return euro *1.08f;
     }
 }
